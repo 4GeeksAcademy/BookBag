660d462b32d455deec4ca92610b915d6ebe13c0c
Cat <126123826+CRob001@users.noreply.github.com>
2023-07-06 18:05:46 +0000

    book bag backend

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,39 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Users(db.Model):
+    __tablename__="users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
+    #discussions = db.relationship("Discussions", backref="created_by")
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<Users {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name, 
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+class Discussions(db.Model):
+    __tablename__="discussions"
+    id=db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    created_by = db.relationship("Users", backref="discussions", foreign_keys=[user_id])
+    discussion=db.Column(db.String(500), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Discussions {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "created_by": self.created_by.serialize(), 
+            "discussion": self.discussion,
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,57 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Users
 from api.utils import generate_sitemap, APIException
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+# login Route-----------------------------------------------------------------------------
+@api.route("/login", methods=["POST"])
+def create_token():
+    if request.method == 'POST':
+      email = request.json.get("email", None)
+      password = request.json.get("password", None)
+      if not email:
+          return jsonify({"msg": "Email is required"}), 400
+      if not password:
+          return jsonify({"msg": "Password is required"}), 400
+      user = Users.query.filter_by(email=email).first()
+      if not user:
+          return jsonify({"msg": "Email/Password are incorrect"}), 401
+      if not check_password_hash(user.password, password):
+          return jsonify({"msg": "Username/Password are incorrect"}), 401
+      # create token
+      expiration = datetime.timedelta(days=3)
+      access_token = create_access_token(identity= user.id, expires_delta= expiration)
+      return jsonify(access_token=access_token)
+    return jsonify(msg="wrong user")
+# create user -----------------------------------------------------------------------------------------------------------
+@api.route('/createUser', methods=['POST'])
+def createUser():
+  if request.method == 'POST':
+    request_body = request.get_json()
+    if not request_body["name"]:
+      return jsonify({"msg": "Name is required"}), 400
+    if not request_body["email"]:
+      return jsonify({"msg": "Email is required"}), 400
+    if not request_body["password"]:
+      return jsonify({"msg": "Password is required"}), 400
+    user = Users.query.filter_by(email=request_body["email"]).first()
+    if user:
+      return jsonify({"msg": "User already exists"}), 400
+    user = Users(
+          name = request_body["name"],
+          email = request_body["email"],
+          password = generate_password_hash(request_body["password"]),
+      )
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"created": "Thanks. Your registration was successfully", "status": "true"}), 200
